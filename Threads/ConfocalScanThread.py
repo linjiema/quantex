a5f258076ddd94cdbf92ef3252bb0b500d917414
Linjie <linjiemahku@gmail.com>
2021-04-29 16:33:40 +0800

    Finish confocal scan thread

--- Threads/ConfocalScanThread.py
+++ Threads/ConfocalScanThread.py
@@ -30,10 +30,10 @@ class ConfocalScanThread(QtCore.QThread):
         y_axis = np.array(y_start, y_end, y_step)
 
         # Send the scanning parameter to stage
-        self._hardware.mover.scaning_setting(channel=1,
-                                             start_point=x_start, end_point=x_end,
-                                             line_rate=4
-                                             )
+        self._hardware.mover.scanning_setting(channel=1,
+                                              start_point=x_start, end_point=x_end,
+                                              line_rate=4
+                                              )
 
         # Scanning process
         for y_points in y_axis:
@@ -42,7 +42,28 @@ class ConfocalScanThread(QtCore.QThread):
                 self._hardware.mover.move_position_single(channel=1, location=x_start)
                 while True:
                     try:
+                        # Init all counting hardware
+                        self._hardware.triggered_location_sensor.init_task()
+                        self._hardware.triggered_counter.init_task()
+                        self._hardware.timer.init_task()
 
+                        # Start scanning
+                        self._hardware.timer.start_timer()
+                        self._hardware.mover.scanning_single_line(channel=1,
+                                                                  start_point=x_start, end_point=x_end,
+                                                                  )
 
+                        # Processing the data
+                        posArr = self._hardware.triggered_location_sensor.get_location_raw_data()
+                        ctsArr = self._hardware.triggered_counter.get_counts_array()
+                    except BaseException as e:
+                        print(e, y_points)
+                        # self._hardware.trigger_counter.counter.close()
+                        continue
+                    break
 
-
+                self.update.emit(y_points, posArr, ctsArr)
+            else:
+                print('Warning: Scanning is stopped!')
+                break
+        self.running = False
