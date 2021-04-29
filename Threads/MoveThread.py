994cc7a29d22201f3cda9c9abc147aad747e8ed0
Linjie <linjiemahku@gmail.com>
2021-04-29 17:41:23 +0800

    Add move thread into the directory

---
+++ Threads/MoveThread.py
@@ -0,0 +1,21 @@
+"""
+This Thread is used to move the position of the stage
+"""
+
+from PyQt5 import QtCore
+import sys
+import time
+
+
+class MoveThread(QtCore.QThread):
+    """
+    Stage moving module as a thread, based on PyQt5.QtCore.QThread
+    parameters:
+    @hardware: python object of hardware control
+    """
+
+    SIGNAL_moved = QtCore.pyqtSignal(float, float, float, name='moved')
+
+    def __init__(self, hardware=None, parent=None):
+        super().__init__(parent)
+        pass
