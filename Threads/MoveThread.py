a3184584e5162353f3d7427620a3059cdba4f01e
Linjie <linjiemahku@gmail.com>
2021-05-03 14:22:21 +0800

    Finish the move Thread Add some todo list

--- Threads/MoveThread.py
+++ Threads/MoveThread.py
@@ -21,4 +21,22 @@ class MoveThread(QtCore.QThread):
         self._hardware = hardware
         self.command = []
 
+    def run(self):
+        """
+        Move to setting position
+        """
+        if len(self.command) == 3:  # The input is a 3 axis value
+            self.move_to_position(self.command)
+
+        # Get position
+        self.x_pos, self.y_pos, self.z_pos = self.get_current_position()
+        self.moved.emit(self.x_pos, self.y_pos, self.z_pos)
+
+    def get_current_position(self):
+        pos = self._hardware.mover.read_position_single(channel=1), self._hardware.mover.read_position_single(channel=2), self._hardware.mover.read_position_single(channel=4)
+        return pos
+
+    def move_to_position(self, command=None):
+        self._hardware.mover.move_position_all(location=command)
+
 
--- todo.md
+++ todo.md
@@ -1,3 +1,4 @@
 - [ ] Start to scan at any line
 - [ ] Change channel to axis
 - [ ] improve the scanning time design
+- [ ] add muti-layer scan part (tem only xy scan)
