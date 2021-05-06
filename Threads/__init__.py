0f173bf4c96b459a39a02a192246fd30d4bda7fd
Linjie <linjiemahku@gmail.com>
2021-05-06 15:04:06 +0800

    Update Thread list

--- Threads/__init__.py
+++ Threads/__init__.py
@@ -4,6 +4,6 @@ This file used to list all the Thread
 
 from Threads.ConfocalScanThread import ConfocalScanThread
 from Threads.CountThread import CountThread
-from DataThread import *
-from MaxThread import MaxThread
-from MoveThread import MoveThread
+from Threads.DataThread import DataThread
+from Threads.MaxThread import MaxThread
+from Threads.MoveThread import MoveThread
