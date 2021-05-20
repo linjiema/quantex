6ccb15b1a2d38e2e93ea38d8d68177698fb3aaed
Linjie <linjiemahku@gmail.com>
2021-05-20 11:38:05 +0800

    Update in environment: Add pytest package

--- Test/test_gramma.py
+++ Test/test_gramma.py
@@ -1,3 +1,5 @@
 import numpy as np
-a = np.array([2] * 3)
-print(a)
+x_start, x_end, x_step, y_start, y_end, y_step = 20.4, 22.4, 0.02, 20.5, 22.5, 0.02
+print(x_start, x_end, x_step, y_start, y_end, y_step)
+x_axis = np.array(x_start, x_end, x_step)
+print(x_axis)
