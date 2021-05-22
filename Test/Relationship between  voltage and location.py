9e36885f0ff4942a63d373b0e8c7229008471675
Linjie <linjiemahku@gmail.com>
2021-05-22 14:13:17 +0800

    perf: give a more accurate location_voltage Relationship
    Now the step between two location has been lower to 0.1um, also the data will be stored in a txt file

--- Test/Relationship between  voltage and location.py
+++ Test/Relationship between  voltage and location.py
@@ -9,7 +9,7 @@ import scipy.optimize
 
 x_start = 0.0
 x_end = 100.0
-x_arr = list(np.arange(0.0, 100.0, 1.0))
+x_arr = list(np.arange(0.0, 100.0, 0.1))
 x_arr.append(100.0)
 
 
@@ -26,7 +26,7 @@ for x_points in x_arr:
     hardware.triggered_location_sensor.init_task()
     hardware.timer.init_task()
 
-    time.sleep(0.1)
+    time.sleep(1)
 
     hardware.timer.start_timer()
 
@@ -43,13 +43,24 @@ print('location: ', x_arr)
 print('Voltage: ', vol_mean)
 print('Std: ', vol_std)
 
-def func(x,a,b):
-    return a*x + b
-p0=[np.max(x_arr)/np.max(vol_mean), 0]
-val, cov = scipy.optimize.curve_fit(func,vol_mean,x_arr,p0)
-print(val)
+p0 = [np.max(x_arr)/np.max(vol_mean), 0]
+val, cov = scipy.optimize.curve_fit(lambda x, a, b: a * x + b, vol_mean, x_arr, p0)
+print('val = ', val)
+
+write_list = []
+for i in range(np.size(x_arr)):
+    write_list.append((x_arr[i], vol_mean[i], vol_std[i]))
+os.chdir(os.path.dirname(__file__))
+f = open("location_voltage_table.txt", "w")
+f.write('The fitted function is' + 'y = ' + str(val[0]) + 'x + ' + str(val[1]) + '\n')
+f.write('location(um) \t voltage_mean(V) \t voltage_std(V) \n')
+for points in write_list:
+    f.write(str(points[0]) + '\t' + str(points[1]) + '\t' + str(points[2]) + '\n')
+f.close()
+
 
 plt.title('Relation of Position-Voltage')
-plt.errorbar(x_arr, vol_mean, yerr=vol_std)
+plt.errorbar(x_arr, vol_mean, color='blue', marker='o', linestyle='none', linewidth=1, markersize=0.5, yerr=vol_std)
+plt.xlabel('Location(um)')
+plt.ylabel('Voltage value(V)')
 plt.show()
-#AllHardware.mover.move_position_single(channel=1, location=x_start)
