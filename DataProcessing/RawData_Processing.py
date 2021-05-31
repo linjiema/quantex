c44ba01fd4afdff65c2ee535782b857acc0bb9df
Linjie <linjiemahku@gmail.com>
2021-05-31 22:07:15 +0800

    feat: plot scanning map
    Now we can plot the scanning map based on the raw data

--- DataProcessing/RawData_Processing.py
+++ DataProcessing/RawData_Processing.py
@@ -1,8 +1,11 @@
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import win32ui
 from collections import deque
 
+
+count_rate = 200
 # Using win GUI to choose the file
 dlg = win32ui.CreateFileDialog(1)
 dlg.SetOFNInitialDir('C:/Users/Desktop')
@@ -28,18 +31,68 @@ with open(path, 'r') as file_raw_data:
 
 # Split the data based on Y value
 diff_y = np.diff(y_arr)
-index_nonzero = np.where(diff_y)
+temp = np.where(diff_y)[0]
+index_nonzero = temp + np.ones(np.size(temp), dtype=int)
 index_nonzero_deq = deque(index_nonzero)
 index_nonzero_deq.appendleft(0)
 
-
+x_2d_arr = []
+y_2d_arr = []
+counts_2d_arr = []
 for i in range(np.size(index_nonzero)):
+    x_2d_arr.append(x_arr[index_nonzero_deq[i]:index_nonzero_deq[i + 1]])
+    y_2d_arr.append(y_arr[index_nonzero_deq[i]:index_nonzero_deq[i + 1]])
+    counts_2d_arr.append(count_arr[index_nonzero_deq[i]:index_nonzero_deq[i + 1]])
 
-#print(x_arr)
-#print(np.size(y_arr_deq_origin))
-#print('new = ', np.size(y_arr_deq_new))
-print(diff_y)
-print(index_nonzero)
-print(np.size(index_nonzero))
-#print('y_arr_deq_new = ', y_arr_deq_new)
-#print(count_arr)
+# Recreate the counts array
+x_max_arr = []
+x_min_arr = []
+counts_2d_arr_plot = []
+
+# Select and reshape counts in single line
+for x_line, counts_line in zip(x_2d_arr, counts_2d_arr):
+
+    x_min = min(x_line)
+    x_max = max(x_line)
+
+    x_reshape_temp = np.arange(x_min, x_max, (x_max - x_min) / 100)
+    x_reshape = np.append(x_reshape_temp, x_max)
+
+    # Select counts based on new x_arr
+    counts_arr_plot_line = []
+    counts_line_arr = np.array(counts_line)
+    for index in range(np.size(x_reshape_temp)):
+        selected_counts = counts_line_arr[np.where((x_line >= x_reshape[index]) & (x_line <= x_reshape[index + 1]))]
+        if np.size(selected_counts) == 0:
+            counts_norm = 0
+        else:
+            counts_norm = (np.sum(selected_counts) / np.size(selected_counts)) * count_rate
+        counts_arr_plot_line.append(counts_norm)
+
+    x_min_arr.append(x_min)
+    x_max_arr.append(x_max)
+    counts_2d_arr_plot.append(counts_arr_plot_line)
+
+# Create the x_arr used for plot
+x_min = np.average(x_min_arr)
+x_max = np.average(x_max_arr)
+
+x_arr_plot_temp = np.arange(x_min, x_max, (x_max - x_min) / 99)
+x_arr_plot = np.append(x_arr_plot_temp, x_max)
+
+# Create the y_arr used for plot
+y_arr_plot = np.array(y_2d_arr)[:, 0]
+
+# Change counts_arr into array
+counts_2d_arr_plot = np.array(counts_2d_arr_plot)
+
+# Plot
+fig = plt.figure(figsize=(12, 10))
+plt.pcolormesh(x_arr_plot, y_arr_plot, counts_2d_arr_plot, shading='nearest')
+plt.xlim(np.min(x_arr_plot), np.max(x_arr_plot))
+plt.ylim(np.min(y_arr_plot), np.max(y_arr_plot))
+plt.colorbar()
+plt.xlabel('X Axis')
+plt.ylabel('Y Axis')
+plt.show()
+
