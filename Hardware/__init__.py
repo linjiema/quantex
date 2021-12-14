9a09ea5382f6a24d5dc932608dc2462dfb784865
Linjie <linjiemahku@gmail.com>
2021-12-14 14:37:10 +0800

    perf: do not load microwave source
    We do not load microwave source anymore as we do not need it in the confocal scan process

--- Hardware/__init__.py
+++ Hardware/__init__.py
@@ -4,7 +4,7 @@ This file used to list all the hardware we use.
 
 # from Hardware.XMT_E70D4 import XMT
 from Hardware.XMT_E70D4_Ser import XMT
-from Hardware.SynthUSB3 import SynthUSB3
+# from Hardware.SynthUSB3 import SynthUSB3
 from Hardware.OpalKelly.API import PulseGenerator
 from Hardware.NI_PCIe_6321 import TriggeredLocationSensor
 from Hardware.NI_PCIe_6321 import TriggeredCounter
@@ -15,7 +15,7 @@ from Hardware.NI_PCIe_6321 import OneTimeCounter_HardwareTimer
 class AllHardware():
     def __init__(self):
         self.mover = XMT()
-        self.mw_source = SynthUSB3
+        # self.mw_source = SynthUSB3()
         self.pulser = PulseGenerator()
         self.triggered_location_sensor = TriggeredLocationSensor()
         self.triggered_counter = TriggeredCounter()
@@ -23,4 +23,10 @@ class AllHardware():
         self.one_time_counter = OneTimeCounter_HardwareTimer()
 
     def cleanup(self):
-        return 0
+        try:
+            self.mover.close_devices()
+            # self.mw_source.clean_up()
+        except BaseException as e:
+            print(e)
+        else:
+            return 0
