c83e7bc0b75db1a493644d9f31a63d9883cae3df
malinjie <linjiemahku@gmail.com>
2024-09-23 14:27:58 +0800

    feat: add logger.py

---
+++ src/utils/logger.py
@@ -0,0 +1,35 @@
+import logging
+import os
+
+# define log file path
+LOG_DIR = "log"
+LOG_FILE = os.path.join(LOG_DIR, "app.log")
+
+# Create log directory if it doesn't exist
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
+
+# create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)  # 设置最低日志级别为 DEBUG
+
+# create formatter
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# create file handler, output to log file
+file_handler = logging.FileHandler(LOG_FILE)
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+# create console handler, output to console
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(formatter)
+
+# add handlers to logger
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+
+# test logger
+logger.debug("Logger setup complete")
