3077d5b5368f518adf46460417f4e9ce3a41f781
Linjie <linjiemahku@gmail.com>
2024-10-03 17:05:58 +0800

    feat: confocal_galvo_scan

---
+++ src/threads/confocal_threads/daq_version/XZScanThread_galvo.py
@@ -0,0 +1,90 @@
+"""
+This Thread used for confocal scanning part
+"""
+
+from PyQt5 import QtCore
+import numpy as np
+import sys
+import time
+
+
+class XZScanThread(QtCore.QThread):
+    """
+    Confocal Scanning module as a thread, based on PyQt5.QtCore.QThread
+
+    """
+    SIGNAL_update = QtCore.pyqtSignal(float, list, list, name='update')
+
+    def __init__(self, hardware=None, parent=None):
+        self._hardware = hardware
+        super().__init__(parent)  # Inherit the init method of parent class
+        self.running = False
+        self.parameters = [0, 65, 0.65, 0, 35, 0.35, 0, 0]
+        # [X_Start, X_End, X_Step, Z_Start, Z_End, Z_Step, Y_Position, Line_Frequency]
+
+    def run(self):
+        self.running = True
+
+        # Move to Goal layer
+        print('move to goal layer')
+        self._hardware.mover.move_position_single(channel=2, location=self.parameters[6])
+
+        # Define the parameters
+        x_start, x_end, x_step, z_start, z_end, z_step = self.parameters[:6]
+        x_axis = np.arange(x_start, x_end, x_step)
+        z_axis = np.arange(z_start, z_end, z_step)
+
+        # Send the scanning parameter to stage
+        wave_forward, wave_back = self._hardware.mover.generating_scan_array(channel=1,
+                                                                             start_point=x_start,
+                                                                             end_point=x_end,
+                                                                             line_rate=1
+                                                                             )
+
+        # Scanning process
+        forward_back_status = True
+        for z_points in z_axis:
+            if self.running:
+                # Move to one location
+                self._hardware.mover.move_position_single(channel=4, location=z_points)
+                while True:
+                    try:
+                        # Init all counting hardware
+                        self._hardware.triggered_location_sensor.init_task()
+                        self._hardware.triggered_counter.init_task()
+                        self._hardware.timer.init_task()
+
+                        # Start scanning
+
+                        self._hardware.mover.move_position_single(channel=1, location=wave_forward[0])
+                        self._hardware.timer.start_timer()
+                        self._hardware.mover.scanning_single_line(channel=1, waveform=wave_forward)
+                        self._hardware.mover.scanning_single_line(channel=1, waveform=wave_back)
+                        '''
+                        if forward_back_status:
+                            self._hardware.mover.move_position_single(channel=1, location=wave_forward[0])
+                            self._hardware.timer.start_timer()
+                            self._hardware.mover.scanning_single_line(channel=1, waveform=wave_forward)
+                        else:
+                            self._hardware.mover.move_position_single(channel=1, location=wave_back[0])
+                            self._hardware.timer.start_timer()
+                            self._hardware.mover.scanning_single_line(channel=1, waveform=wave_back)
+                        '''
+                        # Processing the data
+                        posArr = self._hardware.triggered_location_sensor.get_location_data()
+                        ctsArr = self._hardware.triggered_counter.get_counts_array()
+                        self._hardware.timer.recycle_timer()
+                        forward_back_status = bool(1 - forward_back_status)
+                    except BaseException as e:
+                        print(e, z_points)
+                        self._hardware.triggered_location_sensor.close()
+                        self._hardware.trigger_counter.close()
+                        self._hardware.timer.close()
+                        continue
+                    break
+
+                self.update.emit(z_points, posArr, ctsArr)
+            else:
+                print('Warning: Scanning is stopped!')
+                break
+        self.running = False
